6f21a7c463db73dd81e29067e0665ed9ab158bfd
ahmadilham_13 <ahmad13ilham13@gmail.com>
2023-10-21 20:23:10 +0700

    Update: move navbar folder

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Navbar from '@/components/Utilities/Navbar'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
