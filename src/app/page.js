dbdf3c356b73e267785a492e21249e3cbc299671
ahmadilham_13 <ahmad13ilham13@gmail.com>
2023-10-21 20:45:53 +0700

    Update: list data mapping for dynamic code

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import AnimeList from "@/components/AnimeList"
-import Link from "next/link"
+import Header from "@/components/AnimeList/Header"
 
 const Home = async () => {
 
@@ -8,24 +8,17 @@ const Home = async () => {
   }
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=${params.limit}`)
   
-  const anime = await response.json()
+  const topAnime = await response.json()
 
   return (
-    <div>
-      <div className="flex justify-between items-center p-4">
-        <h1 className="text-2xl font-bold">PALING POPULER</h1>
-        <Link href="/populer" className="md:text-xl text-sm underline hover:text-indigo-500 transition-all">Lihat Semua</Link>
-      </div>
-      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
-        {anime.data.map(data => {
-          return (
-            <div key={data.mal_id} className="shadow-xl">
-              <AnimeList data={data}/>
-            </div>
-          )
-        } )}
-      </div>
-    </div>
+    <>
+      {/* anime populer start */}
+      <section>
+        <Header title="Paling Populer" linkHref="/populer" linkTitle="Lihat Semua"/>
+        <AnimeList api={topAnime} />
+      </section>
+      {/* anime populer end */}
+    </>
   )
 }
 
