7edfb444f8b8b71de5b317658410a794397a9e4f
ahmadilham_13 <ahmad13ilham13@gmail.com>
2023-10-21 21:43:48 +0700

    Update: custom color

--- src/components/AnimeList/Header.js
+++ src/components/AnimeList/Header.js
@@ -3,10 +3,10 @@ import Link from "next/link"
 const Header = ({title, linkHref, linkTitle}) => {
     return (
         <div className="flex justify-between items-center p-4">
-            <h1 className="text-2xl font-bold">{title}</h1>
+            <h1 className="text-2xl font-bold text-customColor-primary">{title}</h1>
             {linkHref && linkTitle 
                 ? 
-                <Link href={linkHref} className="md:text-xl text-sm underline hover:text-indigo-500 transition-all">
+                <Link href={linkHref} className="md:text-xl text-sm underline hover:text-customColor-accent text-customColor-primary transition-all">
                     {linkTitle}
                 </Link>
                 :
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,9 +3,9 @@ import InputSearch from "./InputSearch"
 
 const Navbar = () => {
     return (
-        <header className="bg-indigo-500">
-            <div className="flex md:flex-row flex-col justify-between p-4 gap-2">
-                <Link href="/" className="font-bold text-white text-2xl">MYANIMELIST</Link>
+        <header className="bg-customColor-accent">
+            <div className="flex md:flex-row flex-col justify-between md:items-center p-4 gap-2">
+                <Link href="/" className="font-bold text-2xl">MYANIMELIST</Link>
                 <InputSearch />
             </div>
         </header>
