f439b5929b5ed84e0773fb6e5986a6a2e1b3f62d
ahmadilham_13 <ahmad13ilham13@gmail.com>
2023-12-03 11:22:11 +0700

    Add: UI for dashboard collection

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,15 +1,23 @@
 import {authUserSession} from "@/libs/authLibs"
 import Image from "next/image"
+import Link from "next/link"
 import {redirect} from "next/navigation"
 
 const Page = async() => {
     const user = await authUserSession()
 
     return (
-        <div className="text-customColor-primary">
-            <h3>Dashboard</h3>
-            <h5>Welcome {user.name}</h5>
-            <Image src={user.image} alt="..." width={250} height={250}/>
+        <div className="mt-8 text-customColor-primary flex flex-col justify-center items-center">
+            <h5 className="text-2xl font-bold">Welcome, {user?.name}</h5>
+            <Image src={user?.image} alt="..." width={250} height={250}/>
+            <div className="flex flex-wrap gap-4 py-8">
+                <Link href="/users/dashboard/collection" className="bg-customColor-accent text-customColor-dark font-bold px-4 py-3 text-xl">
+                    My Collection
+                </Link>
+                <Link href="/users/dashboard/comment" className="bg-customColor-accent text-customColor-dark font-bold px-4 py-3 text-xl">
+                    My Comment
+                </Link>
+            </div>
         </div>
     )
 }
---
+++ src/components/Dashboard/Header.jsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { ArrowSquareLeft } from "@phosphor-icons/react"
+import { useRouter } from "next/navigation"
+
+const Header = ({title}) => {
+    const router = useRouter()
+    const handleBack = (event) => {
+        event.preventDefault()
+        router.back()
+    }
+    return (
+        <div className="flex justify-between items-center mb-4">
+            <button className="text-customColor-primary" onClick={handleBack}><ArrowSquareLeft size={32} /></button>
+            <h3 className="text-2xl text-customColor-primary font-bold">{title}</h3>
+        </div>
+    )   
+}
+
+export default Header
